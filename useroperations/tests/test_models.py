2189f71dc638eb709710c2b3c2bd793b6bf67be3
Sven van Crombrugge <s.vancrombrugge@lvgl.saarland.de>
2020-10-16 13:44:23 +0200

    Test ApplicationSliderElement Model

--- useroperations/tests/test_models.py
+++ useroperations/tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 from mixer.backend.django import mixer
 pytestmark = pytest.mark.django_db
 
-from ..models import Navigation
+from ..models import Navigation, ApplicationSliderElement
 
 
 # Tests start here
@@ -14,3 +14,28 @@ class TestNavigation:
     def test_str(self):
         obj = mixer.blend('useroperations.Navigation', name="Home")
         assert str(obj) == "Home"
+
+
+class TestApplicationSliderElement:
+    def test_model(self):
+        obj = mixer.blend('useroperations.ApplicationSliderElement')
+        assert obj.pk == 1
+
+    def test_str(self):
+        obj = mixer.blend('useroperations.ApplicationSliderElement', title="Slider Element 1")
+        assert str(obj) == "Slider Element 1"
+
+    def test_validate_types(self):
+        obj = mixer.blend('useroperations.ApplicationSliderElement')
+        assert type(obj.title) is str
+        assert type(obj.anchor_href) is str
+        assert type(obj.image_src) is str
+
+    def test_create_multiple_objects(self):
+        objects = []
+        for _ in range(20):
+            objects.append(mixer.blend('useroperations.ApplicationSliderElement'))
+        
+        assert type(objects[10].title) is str
+        assert len(objects) == 20
+       
