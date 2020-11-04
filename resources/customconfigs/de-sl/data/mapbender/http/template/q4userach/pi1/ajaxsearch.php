345bc9960907f406bdda7ffca8f9f007311f9764
Sven van Crombrugge <s.vancrombrugge@lvgl.saarland.de>
2020-11-04 17:26:19 +0100

    Remove resources/ and documentation/

--- resources/customconfigs/de-sl/data/mapbender/http/template/q4userach/pi1/ajaxsearch.php
+++
@@ -1,4 +0,0 @@
-<?php
-require("search.common.php");
-$xajax->printJavascript("","typo3conf/ext/q4u_search/pi1/xajax_js/xajax.js");
-?>
--- resources/scripts/update-modsec-rules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# use this in a cronjob every night
-git clone --progress https://github.com/coreruleset/coreruleset /tmp/modsecurity-crs
-cp -a /tmp/modsecurity-crs/rules /usr/share/modsecurity-crs/
-rm -r /tmp/modsecurity-crs
