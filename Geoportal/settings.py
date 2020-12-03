f0b67f4b4c2e5e41d00a250d033667be760bd6be
Sven van Crombrugge <s.vancrombrugge@lvgl.saarland.de>
2020-12-03 13:03:16 +0100

    Update settings.py

--- Geoportal/settings.py
+++ Geoportal/settings.py
@@ -16,30 +16,23 @@ from django.utils.log import DEFAULT_LOGGING
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = "/data/"
+PROJECT_DIR = "/opt/geoportal/"
 SESSION_NAME = 'PHPSESSID'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#m+rso_^a!ii6fg97kd7woxa$ttr&jn^!=_(!wgrukal81q(9+'
+SECRET_KEY = 'k?WG:q@>x+}j~k+Q/k?WG:q@>x+}j~k+Q/,=0Z@kEbv8r/*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 DEFAULT_LOGGING['handlers']['console']['filters'] = []
 
-HOSTNAME = "localhost"
-HTTP_OR_SSL = "http://"
-SEARCH_API_PROTOCOL = "http"
-# HTTPS will fail on self signed certs, uncomment all occurrences of "verify=False"
-# in /opt/GeoPortal.rlp/searchCatalogue/utils/searcher.py if you want to bypass validation
+HOSTNAME = "geoportal.saarland.de"
+HTTP_OR_SSL = "https://"
+SEARCH_API_PROTOCOL = "https"
 
-ALLOWED_HOSTS = [HOSTNAME, '127.0.0.1', 'localhost']
-
-# Recaptcha Config
-USE_RECAPTCHA = 0
-GOOGLE_RECAPTCHA_SECRET_KEY = ''
-GOOGLE_RECAPTCHA_PUBLIC_KEY = ''
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', HOSTNAME]
 
 # Mediawiki
 INTERNAL_PAGES_CATEGORY = "Portalseite"
@@ -52,35 +45,20 @@ IFRAME_WIDTH = 2000
 INTERNAL_SSL = False
 
 # Search module settings
-PRIMARY_CATALOGUE = 3
+PRIMARY_CATALOGUE = 2
 PRIMARY_SRC_IMG = "primary_results.png"
-DE_CATALOGUE = 4
+DE_CATALOGUE = 2
 DE_SRC_IMG = "de_results.png"
-EU_CATALOGUE = 7
+EU_CATALOGUE = 3
 EU_SRC_IMG = "eu_results.png"
 OPEN_DATA_URL = "https://okfn.org/opendata/"
 
-# Mailing settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False
-EMAIL_HOST = 'server.domain.tld'
-EMAIL_HOST_USER = 'geoportal@server.domain.tld'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_PORT = 25
-ROOT_EMAIL_ADDRESS = "geoportal-root@server.domain.tld"
-
 # Gui settings
-MODERN_GUI = "Geoportal-RLP_2019"
-DEFAULT_GUI = "Geoportal-RLP"
-
-# WMC ID that should be loaded on mobile devices
-MOBILE_WMC_ID = None
-# Directory for inspire Downloads
-INSPIRE_ATOM_DIR = "" # eg: "/var/www/html/inspiredownloads/"
-INSPIRE_ATOM_ALIAS = "" # eg: "/inspiredownloads/"
+DEFAULT_GUI = "Geoportal-SL"
+MODERN_GUI = "Geoportal-SL-2020"
 
 # Social networking and news feeds
-TWITTER_NAME = "GeoPortalRLP"
+TWITTER_NAME = ""
 RSS_FILE = "http://www.geoportal.rlp.de" + "/mapbender/geoportal/news/georss.xml"
 
 # Application definition
@@ -116,9 +94,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Geoportal.urls'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Geoportal/static"),  # for generic static files, which are used in all apps
-]
 
 TEMPLATES = [
     {
@@ -156,10 +131,10 @@ DATABASES = {
                     'options': '-c search_path=django,mapbender,public'
                     },
         'NAME':'mapbender',
-        'USER':'mapbenderdbuser',
-        'PASSWORD':'mapbenderdbpassword',
+        'USER':'mapbender',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
-        'PORT':''
+        'PORT':'5432'
     }
 }
 
@@ -217,3 +192,25 @@ LOCALE_PATHS = (
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR + "/static/"
+
+# Mailing settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = False
+EMAIL_HOST = 'mrelay01.saarland.de'
+EMAIL_HOST_USER = 'geoportal.saarland@lvgl.saarland.de'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_PORT = 25
+ROOT_EMAIL_ADDRESS = "root@geoportal.saarland.de"
+EMAIL_CONTACT = 'geoportal.saarland@lvgl.saarland.de'
+
+# Recaptcha Config
+USE_RECAPTCHA = 1
+GOOGLE_RECAPTCHA_SECRET_KEY = ''
+GOOGLE_RECAPTCHA_PUBLIC_KEY = ''
+
+# WMC ID that should be loaded on mobile devices
+MOBILE_WMC_ID = None
+
+# Directory for inspire Downloads
+INSPIRE_ATOM_DIR = "" # eg: "/var/www/html/inspiredownloads/"
+INSPIRE_ATOM_ALIAS = "" # eg: "/inspiredownloads/"
