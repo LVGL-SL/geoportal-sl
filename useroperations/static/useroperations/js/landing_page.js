316592427473a0849fa93735072561ca26e47425
Sven van Crombrugge <s.vancrombrugge@lvgl.saarland.de>
2021-02-10 17:51:16 +0100

    refactor carousel

--- useroperations/static/useroperations/js/landing_page.js
+++ useroperations/static/useroperations/js/landing_page.js
@@ -1,45 +1,93 @@
-const sliderElement = document.querySelector("#landing-page-slider-container");
-sliderElement.addEventListener("mouseenter", () => stopCarousel());
-sliderElement.addEventListener("mouseleave", () => startCarousel());
+document.addEventListener('DOMContentLoaded', () => {
 
+  /* 
+      carousel
+  */
+  const carouselContainer = document.querySelector('#landing-page-carousel-container');
+  const carouselLeftButton = document.querySelector('#btn-carousel-left');
+  const carouselRightButton = document.querySelector('#btn-carousel-right');
+  const carouselCircleButtons = document.querySelectorAll('.carousel-dot');
+
+  carouselContainer.addEventListener('mouseenter', () => stopCarousel());
+  carouselContainer.addEventListener('mouseleave', () => startCarousel());
+  carouselLeftButton.addEventListener('click', () => displayPreviousCarouselElement());
+  carouselRightButton.addEventListener('click', () => displayNextCarouselElement());
+
+  for (btn of carouselCircleButtons) {
+    btn.addEventListener('click', (e) => {
+      const newIndex = parseInt(e.target.dataset.index)
+      displayCarouselElementByIndex(newIndex);
+    });
+  }
+
+  displayCarouselElementByIndex(0);
+  startCarousel();
+
+});
+
+/* 
+  carousel variables and configurations
+*/
+const CAROUSEL_INTERVAL_TIMEOUT_IN_MS = 5000;
+var carouselIndex = 0;
 let carouselInterval;
-startCarousel();
 
-function carousel() {
-  plusDivs(1);
+/* 
+  carousel functions
+*/
+
+function displayNextCarouselElement() {
+
+  const newIndex = parseInt(carouselIndex + 1);
+  displayCarouselElementByIndex(newIndex);
+}
+
+function displayPreviousCarouselElement() {
+
+  const newIndex = parseInt(carouselIndex - 1);
+  displayCarouselElementByIndex(newIndex);
+}
+
+function displayCarouselElementByIndex(index) {
+
+  const carouselImages = document.querySelectorAll('.carousel-image');
+  const carouselDots = document.querySelectorAll('.carousel-dot');
+
+  if (index >= carouselImages.length) {
+    carouselIndex = 0;
+  } else if (index < 0) {
+    carouselIndex = carouselImages.length - 1;
+  } else {
+    carouselIndex = index;
+  }
+
+  hideAllElements(carouselImages);
+  for (dot of carouselDots) {
+    dot.classList.remove("w3-white");
+  }
+
+  const currentImage = carouselImages[carouselIndex];
+  const currentDot = carouselDots[carouselIndex];
+
+  currentImage.style.display = "block";
+  currentDot.classList.add("w3-white");
 }
 
 function startCarousel() {
-  carouselInterval = setInterval(carousel, 5000);
+  carouselInterval = setInterval(displayNextCarouselElement, CAROUSEL_INTERVAL_TIMEOUT_IN_MS);
 }
 
 function stopCarousel() {
   clearInterval(carouselInterval);
 }
 
-var slideIndex = 1;
-showDivs(slideIndex);
+/*
+  Generic functions
+*/
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+function hideAllElements(elements) {
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  for (element of elements) {
+    element.style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
 }
